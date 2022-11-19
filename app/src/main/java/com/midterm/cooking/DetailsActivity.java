8a5bc551c700c9b159446b71ff81861fa90912e2
Thanhdutit <102200356@sv1.dut.udn.vn>
2022-11-19 12:37:41 +0700

    origi project

---
+++ app/src/main/java/com/midterm/cooking/DetailsActivity.java
@@ -0,0 +1,19 @@
+package com.midterm.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+
+    }
+}
---
+++ app/src/main/java/com/midterm/cooking/MainActivity.java
@@ -0,0 +1,26 @@
+package com.midterm.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout btnWelcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnWelcome = findViewById(R.id.btn_welcome);
+        btnWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
